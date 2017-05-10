fc6591e846923c094528aa6fb3dade8c7772898e
ejschwarzkopf <ejschwarzkopf@gmail.com>
2017-05-10 16:41:43 -0700

    Update LDhatter.sh

--- LDhat_map/LDhatter.sh
+++ LDhat_map/LDhatter.sh
@@ -3,16 +3,44 @@
 #It was written for a particular file and folder structure, but this version has been modified to be customizable to anyone's needs.
 #The "infile" and "outfile" need to be replaced with the file name of your choosing for input and output respectively 
 
-n=$1#highest window number to be run
-pop=$2#population
-chr=$3#chromosome
-k=$4#lowest window number to be run
-ipath=$5#input path, remember to end it with a /
-opath=$6#output path
-lkpath=$7#lookup table path
+#n: highest window number to be run
+#pop: population
+#chr: chromosome
+#k: lowest window number to be run
+#ipath: input path, remember to end it with a /
+#opath: output path
+#lk: lookup table file
+
+while getopts “n:k:” OPTION
+do
+  case $OPTION in
+    n)
+      n=$OPTARG
+      ;;
+    k)
+      k=$OPTARG
+      ;;
+    pop)
+      pop=$OPTARG
+      ;;
+    chr)
+      chr=$OPTARG
+      ;;
+    ipath)
+      ipath=$OPTARG
+      ;;
+    opath)
+      opath=$OPTARG
+      ;;
+    lk)
+      lk=$OPTARG
+      ;;
+  esac
+done
+
 
 for i in `seq $k $n`; do#run over sequence of windows
-  interval -seq ${ipath}filename.${pop}.${chr}.${i}.sites -loc ${ipath}infilename.${pop}.${chr}.${i}.locs -lk ${lkpath}lk_${pop} -its 100000000 -bpen 5 -samp 10000 #run interval for 100 million iterations, block penalty of 5 and sampling every 10 thousand iterations
+  interval -seq ${ipath}filename.${pop}.${chr}.${i}.sites -loc ${ipath}infilename.${pop}.${chr}.${i}.locs -lk $lk -its 100000000 -bpen 5 -samp 10000 #run interval for 100 million iterations, block penalty of 5 and sampling every 10 thousand iterations
   mv bounds.txt ${opath}outfilename.${pop}.${chr}.${i}.bounds.txt#move the bounds.txt output file to it's destination
   mv new_lk.txt ${opath}outfilename.${pop}.${chr}.${i}.new_lk.txt#move the new_lk.txt output file to it's destination
   mv rates.txt ${opath}outfilename.${pop}.${chr}.${i}.rates.txt#move the rates.txt output file to it's destination
