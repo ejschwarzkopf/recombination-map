b3d085f939caae7b730529247def25c7e19981a7
ejschwarzkopf <ejschwarzkopf@gmail.com>
2017-05-10 16:58:46 -0700

    Update LDhatter.sh

--- LDhat_map/LDhatter.sh
+++ LDhat_map/LDhatter.sh
@@ -4,12 +4,15 @@
 #The "infile" and "outfile" need to be replaced with the file name of your choosing for input and output respectively 
 
 #n: highest window number to be run
+#k: lowest window number to be run
 #pop: population
 #chr: chromosome
-#k: lowest window number to be run
 #ipath: input path, remember to end it with a /
 #opath: output path
 #lk: lookup table file
+#its: number of iterations
+#bpen: block penalty
+#samp: sample every samp iterations
 
 while getopts “n:k:pop:chr:ipath:opath:lk:” OPTION
 do
@@ -35,12 +38,21 @@ do
     lk)
       lk=$OPTARG
       ;;
+    its)
+      its=$OPTARG
+      ;;
+    bpen)
+      bpen=$OPTARG
+      ;;
+    samp)
+      samp=$OPTARG
+      ;;
   esac
 done
 
 
 for i in `seq $k $n`; do#run over sequence of windows
-  interval -seq ${ipath}filename.${pop}.${chr}.${i}.sites -loc ${ipath}infilename.${pop}.${chr}.${i}.locs -lk $lk -its 100000000 -bpen 5 -samp 10000 #run interval for 100 million iterations, block penalty of 5 and sampling every 10 thousand iterations
+  interval -seq ${ipath}filename.${pop}.${chr}.${i}.sites -loc ${ipath}infilename.${pop}.${chr}.${i}.locs -lk $lk -its $its -bpen $bpen -samp $samp #run interval for $its iterations, block penalty of $bpen and sampling every $samp iterations
   mv bounds.txt ${opath}outfilename.${pop}.${chr}.${i}.bounds.txt#move the bounds.txt output file to it's destination
   mv new_lk.txt ${opath}outfilename.${pop}.${chr}.${i}.new_lk.txt#move the new_lk.txt output file to it's destination
   mv rates.txt ${opath}outfilename.${pop}.${chr}.${i}.rates.txt#move the rates.txt output file to it's destination
